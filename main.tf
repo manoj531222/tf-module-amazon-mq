527458169e2fd12da52588a5985d84a4c1856bf7
manoj <gandhamanojkutty@gmail.com>
2023-09-09 20:02:14 +0530

    dbs

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "main" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "rabbitmq"
+    description = "RABBITMQ"
     from_port   = var.port_no
     to_port     = var.port_no
     protocol    = "tcp"
@@ -31,19 +31,21 @@ resource "aws_security_group" "main" {
 }
 
 
+
 resource "aws_instance" "rabbitmq" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id = var.subnets[0]
-  vpc_security_group_ids = aws_security_group.main.id
-  tags = merge(var.tags, { Name = "${var.name}-${var.env}-rabbitmq" })
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnets[0]
+  vpc_security_group_ids = [aws_security_group.main.id]
+  tags                   = merge(var.tags, { Name = "${var.name}-${var.env}" })
+
   root_block_device {
-    encrypted = true
+    encrypted  = true
     kms_key_id = var.kms_arn
   }
+
+  user_data = base64encode(templatefile("${path.module}/userdata.sh", {
+    rabbitmq_appuser_password = data.aws_ssm_parameter.rabbitmq_appuser_password.value
+  }))
+
 }
-
-user_data = base64encode(templatefile("${path.module}/userdata.sh", {
-  rabbitmq_appuser_password = data.aws_ssm_parameter.rabbitmq_appuser_password.value
-}))
-
